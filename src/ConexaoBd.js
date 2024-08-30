b6a5bbab7e5d18e02a066960c0c65b9fb98621a4
Dev-Cordeiro <viniciusluzcordeiro00@gmail.com>
2024-08-30 17:27:05 -0300

    feat(.Env): criando arquivo env que contem as chaves do projeto para uma melhor segurança

--- src/ConexaoBd.js
+++ src/ConexaoBd.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dtrlrsrmizglyozueoeu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0cmxyc3JtaXpnbHlvenVlb2V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ0NTMzNjgsImV4cCI6MjA0MDAyOTM2OH0.fBmH_quemgX6K9zyhbShHh3TOjlSHCje5RqU7Ug4JyQ';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
